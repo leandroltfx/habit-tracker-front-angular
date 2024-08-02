5cab395b9d4f48382620f4ef0d848760a6cc3680
leandroltfx <leandrobsb55@hotmail.com>
2024-08-01 21:27:25 -0300

    criando formulário de login

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'ht-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm!: FormGroup;
+  public hiddenPassword: boolean = false;
+
+  constructor(
+    private readonly _formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this._buildLoginForm();
+  }
+
+  private _buildLoginForm(): FormGroup {
+    return this._formBuilder.group({
+      email: null,
+      password: null,
+    });
   }
 
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
@@ -10,7 +17,15 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    ReactiveFormsModule,
+
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+
+    LoginRoutingModule,
   ]
 })
 export class LoginModule { }
