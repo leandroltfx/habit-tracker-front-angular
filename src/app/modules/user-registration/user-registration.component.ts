59fc9418c4e272b52403aebd70e2df3c88a57d5e
leandroltfx <leandrobsb55@hotmail.com>
2024-08-04 09:35:52 -0300

    criando formulário de cadastro de usuário

--- src/app/modules/user-registration/user-registration.component.ts
+++ src/app/modules/user-registration/user-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ht-user-registration',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRegistrationComponent implements OnInit {
 
-  constructor() { }
+  public userRegistrationForm!: FormGroup;
+  public hiddenPassword: boolean = false;
+  public hiddenConfirmPassword: boolean = false;
+  public minLengthUsername: number = 3;
+  public maxLengthUsername: number = 30;
+  public minLengthPassword: number = 8;
+  public maxLengthPassword: number = 80;
+
+  private _emailPattern: RegExp = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+  private _usernamePattern: RegExp = /^[a-zA-Z0-9]+$/;
+
+  constructor(
+    private readonly _formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.userRegistrationForm = this._buildUserRegistrationForm();
   }
+
+  public registerUser(): void {
+    console.log(this.userRegistrationForm.controls);
+  }
+
+  public updateConfirmValidator(): void {
+    Promise.resolve().then(() => this.userRegistrationForm.controls['confirmPassword'].updateValueAndValidity());
+  }
+
+  private _buildUserRegistrationForm(): FormGroup {
+    return this._formBuilder.group({
+      username: [null, [Validators.required, Validators.pattern(this._usernamePattern), Validators.minLength(this.minLengthUsername), Validators.maxLength(this.maxLengthUsername)]],
+      email: [null, [Validators.required, Validators.pattern(this._emailPattern)]],
+      password: [null, [Validators.required, Validators.minLength(this.minLengthPassword), Validators.maxLength(this.maxLengthPassword)]],
+      confirmPassword: [null, [Validators.required, this._confirmationValidator]],
+    });
+  }
+
+  private _confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.userRegistrationForm.controls['password'].value) {
+      return { confirm: true, error: true };
+    }
+    return {};
+  };
 
 }
--- src/app/modules/user-registration/user-registration.module.ts
+++ src/app/modules/user-registration/user-registration.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 import { UserRegistrationRoutingModule } from './user-registration-routing.module';
 import { UserRegistrationComponent } from './user-registration.component';
@@ -13,6 +20,13 @@ import { UserRegistrationService } from './acl/service/user-registration.service
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
 
     UserRegistrationRoutingModule,
   ],
