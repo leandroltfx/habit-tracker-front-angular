db1dc0a66461cf344995d7798ca6ed1041344fee
leandroltfx <leandrobsb55@hotmail.com>
2024-09-03 21:45:35 -0300

    importando módulos para testes unitários do login

--- src/app/modules/login/login.component.spec.ts
+++ src/app/modules/login/login.component.spec.ts
@@ -1,4 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 import { LoginComponent } from './login.component';
 
@@ -8,7 +16,17 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+
+        MatCardModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+      ],
     })
     .compileComponents();
 
