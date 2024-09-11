4165b8e409eea7126b5c88e25aa7ee67050f3096
leandroltfx <leandrobsb55@hotmail.com>
2024-09-11 19:34:51 -0300

    iniciando módulo de cadastro de usuário

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
+  { path: 'user-registration', loadChildren: () => import('./modules/user-registration/user-registration.module').then(m => m.UserRegistrationModule) },
   { path: '**', redirectTo: 'login' },
 ];
 
--- src/app/modules/login/login.component.spec.ts
+++ src/app/modules/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -27,6 +28,7 @@ describe('LoginComponent', () => {
       imports: [
         BrowserAnimationsModule,
         ReactiveFormsModule,
+        RouterTestingModule,
 
         MatCardModule,
         MatButtonModule,
