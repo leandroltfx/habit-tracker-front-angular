2d13e34725b6dacdaa82018272cc1f8e8869bd8c
leandroltfx <leandrobsb55@hotmail.com>
2024-09-10 22:19:35 -0300

    criando teste unitário para a MessageService

---
+++ src/app/shared/tests/stubs/mat-snackbat.stub.ts
@@ -0,0 +1,9 @@
+import { MatSnackBarConfig } from "@angular/material/snack-bar";
+
+export class MatSnackBarStub {
+
+    public open(
+        message: string, action?: string, config?: MatSnackBarConfig
+    ): void {}
+
+};
