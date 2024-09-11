0d26764602e93baac6847e08b0051a1b3a570862
leandroltfx <leandrobsb55@hotmail.com>
2024-09-11 15:45:00 -0300

    corrigindo stubs

--- src/app/shared/tests/stubs/login-adapter.service.stub.ts
+++ src/app/shared/tests/stubs/login-adapter.service.stub.ts
@@ -1,17 +1,23 @@
-import { LoginRequestContract } from "../../contracts/request/login/login-request.contract";
-import { LoginResponseDto } from "../../dto/login/login-response.dto";
 import loginResponseMock from "../mocks/login-response.mock";
+import { LoginRequestContract } from "../../contracts/request/login/login-request.contract";
+import { LoginResponseContract } from "../../contracts/response/login/login-response.contract";
+import { LoginResponseDto } from "../../dto/login/login-response.dto";
 
 export class LoginAdapterServiceStub {
 
-    public toLoginRequestContract() {
+    public toLoginRequestContract(
+        email: string,
+        password: string,
+    ) {
         const loginRequestContract = new LoginRequestContract();
         loginRequestContract.email = 'admin@email.com';
         loginRequestContract.password = 'admin123';
         return loginRequestContract;
     }
 
-    public toLoginResponseDto() {
+    public toLoginResponseDto(
+        loginResponseContract: LoginResponseContract
+    ) {
         const loginResponseDto: LoginResponseDto = new LoginResponseDto();
         loginResponseDto.message = loginResponseMock.message;
         loginResponseDto.loggedUser = loginResponseMock.loggedUser;
--- src/app/shared/tests/stubs/login-proxy.service.stub.ts
+++ src/app/shared/tests/stubs/login-proxy.service.stub.ts
@@ -1,10 +1,13 @@
 import { of } from "rxjs";
 
 import loginResponseMock from "../mocks/login-response.mock";
+import { LoginRequestContract } from "../../contracts/request/login/login-request.contract";
 
 export class LoginProxyServiceStub {
 
-    public login() {
+    public login(
+        loginRequestContract: LoginRequestContract
+    ) {
         return of(loginResponseMock);
     }
 
