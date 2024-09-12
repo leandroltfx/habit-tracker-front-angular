87c92f73da0dfb655b72e376c023e16e816795bc
leandroltfx <leandrobsb55@hotmail.com>
2024-09-12 09:33:09 -0300

    criando testes unitários para a camada ACL do módulo de cadastro de usuário

--- src/app/modules/user-registration/acl/proxy/user-registration-proxy.service.spec.ts
+++ src/app/modules/user-registration/acl/proxy/user-registration-proxy.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { UserRegistrationProxyService } from './user-registration-proxy.service';
+import { RegisterUserRequestContract } from 'src/app/shared/contracts/request/register-user/register-user-request.contract';
+import { RegisterUserResponseContract } from 'src/app/shared/contracts/response/register-user/register-user-response.contract';
 
 describe('UserRegistrationProxyService', () => {
   let service: UserRegistrationProxyService;
@@ -20,5 +22,23 @@ describe('UserRegistrationProxyService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('registerUser - deve devolver um objeto do tipo RegisterUserResponseContract', () => {
+
+    const registerUserRequestContract: RegisterUserRequestContract = new RegisterUserRequestContract();
+    registerUserRequestContract.username = 'admin';
+    registerUserRequestContract.email = 'admin@email.com';
+    registerUserRequestContract.password = 'admin123';
+
+    service.registerUser(registerUserRequestContract).subscribe(
+      registerUserResponseContract => {
+        expect(registerUserResponseContract instanceof RegisterUserResponseContract).toBeTrue();
+        expect(registerUserResponseContract.message).toBe('Usuário cadastrado com sucesso!');
+        expect(registerUserResponseContract.loggedUser.id).toBe(1);
+        expect(registerUserResponseContract.loggedUser.username).toBe('Admin');
+        expect(registerUserResponseContract.loggedUser.email).toBe('admin@email.com');
+      }
+    );
   });
 });
--- src/app/modules/user-registration/acl/service/user-registration.service.spec.ts
+++ src/app/modules/user-registration/acl/service/user-registration.service.spec.ts
@@ -5,6 +5,8 @@ import { UserRegistrationAdapterService } from '../adapter/user-registration-ada
 import { UserRegistrationProxyService } from '../proxy/user-registration-proxy.service';
 import { UserRegistrationAdapterServiceStub } from '../../../../shared/tests/stubs/user-registration-adapter.service.stub';
 import { UserRegistrationProxyServiceStub } from '../../../../shared/tests/stubs/user-registration-proxy.service.stub';
+import { RegisterUserResponseDto } from 'src/app/shared/dto/register-user/register-user-response.dto';
+import { LoggedUser } from 'src/app/shared/models/logged-user/logged-user.model';
 
 describe('UserRegistrationService', () => {
   let service: UserRegistrationService;
@@ -22,5 +24,19 @@ describe('UserRegistrationService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('registerUser, deve retornar um objeto do tipo RegisterUserResponseDto', () => {
+
+    service.registerUser('username', 'email@email.com', 'abc123').subscribe(
+      registerUserResponseDto => {
+        expect(registerUserResponseDto instanceof RegisterUserResponseDto);
+        expect(registerUserResponseDto.message).toBe('Usuário cadastrado com sucesso!');
+        expect(registerUserResponseDto.loggedUser instanceof LoggedUser).toBeTrue();
+        expect(registerUserResponseDto.loggedUser.id).toBe(1);
+        expect(registerUserResponseDto.loggedUser.username).toBe('Admin');
+        expect(registerUserResponseDto.loggedUser.email).toBe('admin@email.com');
+      }
+    );
   });
 });
