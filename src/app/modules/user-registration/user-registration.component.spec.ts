e6f5b57daa823fc537d97da5febb45d2aebdf384
leandroltfx <leandrobsb55@hotmail.com>
2024-09-12 09:42:46 -0300

    criando testes unitários para o formulário de cadastro de usuário

--- src/app/modules/user-registration/user-registration.component.spec.ts
+++ src/app/modules/user-registration/user-registration.component.spec.ts
@@ -19,6 +19,9 @@ describe('UserRegistrationComponent', () => {
   let component: UserRegistrationComponent;
   let fixture: ComponentFixture<UserRegistrationComponent>;
 
+  let userRegistrationServiceStub: UserRegistrationServiceStub;
+  let messageServiceStub: MessageServiceStub;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ UserRegistrationComponent ],
@@ -42,10 +45,94 @@ describe('UserRegistrationComponent', () => {
 
     fixture = TestBed.createComponent(UserRegistrationComponent);
     component = fixture.componentInstance;
+
+    userRegistrationServiceStub = TestBed.inject(UserRegistrationService) as unknown as UserRegistrationServiceStub;
+    messageServiceStub = TestBed.inject(MessageService) as unknown as MessageServiceStub;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('registerUser - deve disparar mensagem de sucesso de cadastro de usuário', () => {
+
+    const spyRegisterUser = spyOn(userRegistrationServiceStub, 'registerUser').and.callThrough();
+    const spyShowMessage = spyOn(messageServiceStub, 'showMessage').and.callThrough();
+
+    component.userRegistrationForm.controls['username'].setValue('admin');
+    component.userRegistrationForm.controls['email'].setValue('admin@email.com');
+    component.userRegistrationForm.controls['password'].setValue('admin123#');
+    component.userRegistrationForm.controls['confirmPassword'].setValue('admin123#');
+
+    component.registerUser();
+
+    expect(spyRegisterUser).toHaveBeenCalledWith('admin', 'admin@email.com', 'admin123#');
+    expect(spyShowMessage).toHaveBeenCalledWith('Usuário cadastrado com sucesso!', 'success');
+  });
+
+  it('registerUser - deve impedir o cadastro de usuário se username estiver inválido', () => {
+
+    const spyRegisterUser = spyOn(userRegistrationServiceStub, 'registerUser').and.callThrough();
+    const spyShowMessage = spyOn(messageServiceStub, 'showMessage').and.callThrough();
+
+    component.userRegistrationForm.controls['username'].setValue('user name');
+    component.userRegistrationForm.controls['email'].setValue('admin@email.com');
+    component.userRegistrationForm.controls['password'].setValue('admin123#');
+    component.userRegistrationForm.controls['confirmPassword'].setValue('admin123#');
+
+    component.registerUser();
+
+    expect(spyRegisterUser).not.toHaveBeenCalled();
+    expect(spyShowMessage).not.toHaveBeenCalled();
+  });
+
+  it('registerUser - deve impedir o cadastro de usuário se email estiver inválido', () => {
+
+    const spyRegisterUser = spyOn(userRegistrationServiceStub, 'registerUser').and.callThrough();
+    const spyShowMessage = spyOn(messageServiceStub, 'showMessage').and.callThrough();
+
+    component.userRegistrationForm.controls['username'].setValue('username');
+    component.userRegistrationForm.controls['email'].setValue('admin@');
+    component.userRegistrationForm.controls['password'].setValue('admin123#');
+    component.userRegistrationForm.controls['confirmPassword'].setValue('admin123#');
+
+    component.registerUser();
+
+    expect(spyRegisterUser).not.toHaveBeenCalled();
+    expect(spyShowMessage).not.toHaveBeenCalled();
+  });
+
+  it('registerUser - deve impedir o cadastro de usuário se a senha estiver inválida', () => {
+
+    const spyRegisterUser = spyOn(userRegistrationServiceStub, 'registerUser').and.callThrough();
+    const spyShowMessage = spyOn(messageServiceStub, 'showMessage').and.callThrough();
+
+    component.userRegistrationForm.controls['username'].setValue('username');
+    component.userRegistrationForm.controls['email'].setValue('admin@email.com');
+    component.userRegistrationForm.controls['password'].setValue('admin123');
+    component.userRegistrationForm.controls['confirmPassword'].setValue('admin123');
+
+    component.registerUser();
+
+    expect(spyRegisterUser).not.toHaveBeenCalled();
+    expect(spyShowMessage).not.toHaveBeenCalled();
+  });
+
+  it('registerUser - deve impedir o cadastro de usuário se a confirmação da senha estiver errada', () => {
+
+    const spyRegisterUser = spyOn(userRegistrationServiceStub, 'registerUser').and.callThrough();
+    const spyShowMessage = spyOn(messageServiceStub, 'showMessage').and.callThrough();
+
+    component.userRegistrationForm.controls['username'].setValue('username');
+    component.userRegistrationForm.controls['email'].setValue('admin@email.com');
+    component.userRegistrationForm.controls['password'].setValue('admin123#');
+    component.userRegistrationForm.controls['confirmPassword'].setValue('admin123$$$');
+
+    component.registerUser();
+
+    expect(spyRegisterUser).not.toHaveBeenCalled();
+    expect(spyShowMessage).not.toHaveBeenCalled();
   });
 });
